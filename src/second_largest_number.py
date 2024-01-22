80eb35864ead99acbcf93bcdc798fb02e50e9ca8
Piotr Jurczenko <16348752+pjurczen@users.noreply.github.com>
2024-01-22 13:07:13 +0100

    improve SecondLargestNumber comparisons from 2n - 1 to n + logn - 2

--- src/second_largest_number.py
+++ src/second_largest_number.py
@@ -1,20 +1,22 @@
 
 class SecondLargestNumber:
     def find_second_largest_number(self, numbers: []) -> int:
-        largest_number = self.find_largest_number(numbers)
+        largest_number, history = self.find_largest_number(numbers)
         second_largest_number = float("-inf")
-        for i in numbers:
-            if (i < largest_number) & (i > second_largest_number):
+        for i in history:
+            if i > second_largest_number:
                 second_largest_number = i
         return second_largest_number
 
-    def find_largest_number(self, numbers: []) -> int:
+    def find_largest_number(self, numbers: []) -> (int, []):
         if len(numbers) == 1:
-            return numbers[0]
+            return numbers[0], []
         half_n = int(len(numbers) / 2)
         left_half = numbers[0:half_n]
         right_half = numbers[half_n:]
-        left_result = self.find_largest_number(left_half)
-        right_result = self.find_largest_number(right_half)
-        largest_number = max(left_result, right_result)
-        return largest_number
+        left_result, left_history = self.find_largest_number(left_half)
+        right_result, right_history = self.find_largest_number(right_half)
+        if left_result > right_result:
+            return left_result, left_history + [right_result]
+        else:
+            return right_result, right_history + [left_result]
