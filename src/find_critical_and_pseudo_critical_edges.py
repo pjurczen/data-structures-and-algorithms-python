29b2c0d53f5f2d473af2f737e3201fa142a4459a
Piotr Jurczenko <16348752+pjurczen@users.noreply.github.com>
2024-04-10 07:40:04 +0200

    Write-up

--- src/find_critical_and_pseudo_critical_edges.py
+++ src/find_critical_and_pseudo_critical_edges.py
@@ -1,3 +1,8 @@
 class FindCriticalAndPseudoCriticalEdges:
     def findCriticalAndPseudoCriticalEdges(self, n: int, edges: list[list[int]]) -> list[list[int]]:
-        pass 
+        pass
+        # do normal MST algorithm
+
+        # first occurence of connecting a new node -> add to critical edges
+
+        # second occurence of connecting a node with same weight -> move from critical edge to pseudo-critical edges
